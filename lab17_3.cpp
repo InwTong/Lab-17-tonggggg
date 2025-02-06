ba22216049779480afd4d5fbdcc8a7813018a1bb
InwTong <Tachin_s@cmu.ac.th>
2025-02-06 22:39:15 +0700

    Update lab17_3.cpp

--- lab17_3.cpp
+++ lab17_3.cpp
@@ -4,25 +4,32 @@
 #include<vector>
 using namespace std;
 
-______________ randomVector(unsigned int N){	
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<vector>
+using namespace std;
+
+vector<int> randomVector(unsigned int N){	
     vector<int> v;
-    for(unsigned int i = 0; i < N; i++) v.______________(rand()%10);
-    ______________;
+    for(unsigned int i = 0; i < N; i++) v.push_back(rand()%10);
+    return v;
 }
 
-______________ showVector(______________){
+void showVector(const vector<int>& vec){
 	cout << "[";
-	for(unsigned int i = 0; _____________________; i++){
-		cout << _________________;
-		if(_________________) cout << "]";
+	for(unsigned int i = 0; i < vec.size(); i++){
+		cout << vec[i];
+		if(i == vec.size() - 1) cout << "]";
 		else cout << " ";
 	}
 }
 
-_________________ dotProduct(_____________________________){
+
+int dotProduct(const vector<int>& a, const vector<int>& b){
 	int sum = 0;
-	for(unsigned int i = 0; _____________________; i++) sum += _________________;	
-	_________________;
+	for(unsigned int i = 0; i < a.size() ; i++) sum += a[i] * b[i];	
+	return sum;
 }
 
 int main(){
